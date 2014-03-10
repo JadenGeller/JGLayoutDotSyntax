8894d2557da7aede43ae9da997ae3ced6f45825a
Jaden Geller <jaden.geller@gmail.com>
2014-03-09 19:26:53 -0700

    updated documentation

--- LayoutDotSyntaxExample/JGDynamicSizeLabel.h
+++ LayoutDotSyntaxExample/JGDynamicSizeLabel.h
@@ -10,7 +10,7 @@
 
 @interface JGDynamicSizeLabel : UILabel
 
-/** The font size of the label. Used as a setter, this creates and adds a "constraint" based upon the specification of the assigned JGLayoutParameter. Can only be used with the width and the height attribute. Used as a getter, will return any associated constraint. If none exists, will return nil.
+/** The font size of the label. Used as a setter, this creates and adds a "constraint" based upon the specification of the assigned JGLayoutParameter. The label's original font size is ignored, and replaced by this constraint. Can only be used with the width and the height attribute. Used as a getter, will return any associated constraint. If none exists, will return nil.
  
  @code
  
