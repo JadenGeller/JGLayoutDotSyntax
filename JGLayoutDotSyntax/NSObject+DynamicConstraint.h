9576929e84f57c49e7630de5a3eebe2c23a96ebf
Jaden Geller <jaden.geller@gmail.com>
2014-03-19 21:29:29 -0700

    Documented Dynamic Constraint

--- JGLayoutDotSyntax/NSObject+DynamicConstraint.h
+++ JGLayoutDotSyntax/NSObject+DynamicConstraint.h
@@ -12,6 +12,17 @@
 
 @interface NSObject (DynamicConstraint)
 
+/** Returns a constraint associated with the given key-value path. Reciever must be KVO compliant (if you use properties, it should be!). Whenever the property changes in value, the constraint will automatically update.
+ 
+ @code
+ 
+ view.height = [downloader constraintForKeyPath: @"progress"]; // sets height of view equal to value of downloader.progress, and updates height of view whenever downloader.progress changes
+ 
+ @endcode
+ 
+ @see https://github.com/JadenGeller/JGLayoutDotSyntax for more information.
+ 
+ */
 -(JGLayoutParameter*)constraintForKeyPath:(NSString*)keyPath;
 
 @end
