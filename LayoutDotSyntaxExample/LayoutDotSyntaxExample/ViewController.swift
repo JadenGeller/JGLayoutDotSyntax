3e894cabc6c66b813b8b5f6b37c7db2076c69295
Nicolas Gomollon <nicolasgomollon@yahoo.com>
2014-06-22 00:09:41 -0700

    Added LayoutDotSyntaxExample project.

---
+++ LayoutDotSyntaxExample/LayoutDotSyntaxExample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  LayoutDotSyntaxExample
+//
+//  Objective-C code Copyright (c) 2014 Jaden Geller. All rights reserved.
+//  Swift adaptation Copyright (c) 2014 Nicolas Gomollon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+}
+
